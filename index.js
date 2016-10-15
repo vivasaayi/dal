cc6da2bbef18371279b5afad24739fa3516c404e
Rajan Panneer Selvam <vivasaayi@gmail.com>
2016-10-15 13:58:57 +0530

    Exposed the Importer and Exporter

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 const MongoWrapper = require("./src/mongo-wrapper");
 const MongoConfig = require("./src/mongo-config");
+const Exporter = require("./src/exporter");
+const Importer = require("./src/importer");
 
 module.exports = {
-    MongoWrapper,
-    MongoConfig
+  MongoWrapper,
+  MongoConfig,
+  Exporter,
+  Importer
 };
